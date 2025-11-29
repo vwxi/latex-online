16ef190398d1eec1b2224a65c1cde409f7e3b21d
Noah Vezina <noahvezina26@gmail.com>
2025-11-29 14:35:40 -0500

    event listener for compile

--- static/editor.js
+++ static/editor.js
@@ -10,13 +10,20 @@ function updateDisplay() {
 
     const re_command = /\\([A-Za-z0-9@]+)?/g;
     let result = textEditor.value.replaceAll(re_command, (match) => {
-        return "<span style=\"color: blue;\">" + match + "</span>"
+        return "<span style=\"color: blue;\">" + match + "</span>";
     });
 
     const re_comment = /^%.*$/gm;
     result = result.replaceAll(re_comment, (match) => {
-        return "<span style=\"color: green;\">" + match + "</span>"
+        return "<span style=\"color: green;\">" + match + "</span>";
     });
 
     codeDisplay.innerHTML = result;
 }
+
+
+const compileButton = document.getElementById("compile");
+compileButton.addEventListener("click", e => {
+    console.log(textEditor.value)
+});
+
