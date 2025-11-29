11e23f10877d3c6ed70ff51405446a6c2eefe1fe
AlerGames <mehmetaliakdeniz21@gmail.com>
2025-11-29 15:22:10 -0500

    lol

--- static/js/image-upload.js
+++ static/js/image-upload.js
@@ -21,7 +21,29 @@ function addImage(){
     imageFileNames.push(fetch('/add', {
       method: 'POST',
       body: data
+<<<<<<< Updated upstream
     }));
+=======
+    }).then((response) => response.json())
+      .then((val) => imageFileNames.push(val)))();
+
+    imageFileName = imageFileNames[length(imageFileNames) - 1]
+
+    const imageDisplaySpan = document.createElement("span");
+    const imageDisplayButton = document.createElement("button");
+    const imageDisplayText = document.createElement("p");
+
+    imageDisplayText.innerText = imageFileName;
+
+    imageDisplayButton.innerText = 'Delete Image';
+    imageDisplayButton.type = 'button';
+    imageDisplayButton.
+
+    imageDisplaySpan.appendChild(imageDisplayText);
+    imageDisplaySpan.appendChild(imageDisplayButton);
+    
+    imageFileContainer.appendChild(imageDisplaySpan);
+>>>>>>> Stashed changes
 }
 
 function removeImage(imageFileName){
